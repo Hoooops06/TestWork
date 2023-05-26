5f695f9229003a06a1a7761179147c81c3facf0c
AleksandrM <sashkaanna03@gmail.com>
2023-05-26 23:17:47 +0300

    Нахождение максимума

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear(); // Clear
+Console.Write("Введите 1-ое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
