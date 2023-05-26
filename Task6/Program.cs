09975241a640036a92332f7b6c29f3bf2cce5d30
AleksandrM <sashkaanna03@gmail.com>
2023-05-27 00:10:20 +0300

    Наконец-то победил эту задачу

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear(); // Clear
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 1)
+{
+    Console.WriteLine($"Число: {a} нечетное");
+}
+
+else
+{
+    Console.WriteLine($"Число: {a} четное");
+}
