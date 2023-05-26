d4cd257ccbb4f4195f91198de0b03a0dc674d1aa
AleksandrM <sashkaanna03@gmail.com>
2023-05-26 23:21:12 +0300

    Задание 4 с тремя цифрами

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear(); // Clear
+Console.Write("Введите 1-ое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите 3-ое число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
