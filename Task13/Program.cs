cbdb1d30fc98c84b630aa1f5ef851abdc7ce4e43
AleksandrM <sashkaanna03@gmail.com>
2023-05-28 09:17:36 +0300

    Третья цифра числа

---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear(); // Clear
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a < 100)
+    Console.Write("Третьей цифры нет");
+else
+{
+    while (a > 999)
+        a = a / 10;
+    Console.Write($"{a % 10}");
+}
