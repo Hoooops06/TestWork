f2908d3c74ba033bd108280a2cd362f903484e0d
AleksandrM <sashkaanna03@gmail.com>
2023-05-28 08:39:08 +0300

    Второе число из 3х значного

---
+++ Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear(); // Clear
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a < 100 || a > 999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    int b = a / 10;
+    int n2 = b % 10;
+    Console.WriteLine($"Второе число: {n2} ");
+}
+
