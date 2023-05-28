0b3501785bfb5574fda5613265490086b606a194
AleksandrM <sashkaanna03@gmail.com>
2023-05-28 09:57:53 +0300

    День недели

---
+++ Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear(); // Clear
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+while ( a < 1 || a > 7 )
+
+{
+    Console.Write("Такого дня недели нет.\nВведите день недели(от 1 до 7): ");
+    a = int.Parse(Console.ReadLine()!);
+}
+
+if (a < 6)
+
+Console.Write("Будний день");
+
+else 
+{
+Console.Write("Выходной день");
+}
