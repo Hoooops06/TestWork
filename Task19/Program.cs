fa31b959a4831b525506a5822599b8f935e667d3
AleksandrM <sashkaanna03@gmail.com>
2023-06-03 09:11:28 +0300

    Полиндром

---
+++ Task19/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 9999 || n > 100000)
+{
+    Console.Write("Вы ошиблись!\nВведите пятизначное число: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+string number = n.ToString();
+if (number[0] == number[4] && number[1] == number[3])
+Console.Write($"Число {n} является палиндромом");
+else
+    Console.Write($"Число {n} не является палиндромом");
