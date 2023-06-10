f087492234d44424dcf13916f31f624c3f12ef92
AleksandrM <sashkaanna03@gmail.com>
2023-06-10 10:09:19 +0300

    точка пересечения двух прямых

---
+++ Task43/Program.cs
@@ -0,0 +1,20 @@
+﻿static void Program()
+{
+    double b1 = 2;
+    double k1 = 5;
+
+    double b2 = 4;
+    double k2 = 9;
+
+    double x;
+    double y;
+
+    void ProgramMath()
+    {
+        x = (b2 - b1) / (k1 - k2);
+        y = k1 * x + b1;
+        Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2}: \nresult = ( x {x}; y {y})");
+    }
+    ProgramMath();
+}
+Program();
