5f862f3a7d05ac83e4106c1fd0ad3e2a08dc38f8
AleksandrM <sashkaanna03@gmail.com>
2023-06-10 10:04:43 +0300

    сколько чисел больше 0

---
+++ Task41/Program.cs
@@ -0,0 +1,25 @@
+﻿string textPrintStart = "Введите число через пробел и запятую";
+Console.Write(textPrintStart.Insert(36, ": "));
+
+char[] singEliments = new char[3] { ' ', ',', '.' };
+string[] userEnter = Console.ReadLine()!.Split(singEliments, StringSplitOptions.RemoveEmptyEntries);
+
+int[] array = new int[userEnter.Length];
+
+for (int i = 0; i < array.Length; i++)
+    int.TryParse(userEnter[i], out array[i]);
+
+int counter = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+    if (array[i] > 0)
+        counter++;
+}
+
+string textPrintFinish = "Постичайте сколько, чисел больше 0, ввёл пользователь.";
+
+string[] textPrint = textPrintFinish.Split(", ");
+
+Console.Write($"\n{textPrint[1]} -> {counter}");
